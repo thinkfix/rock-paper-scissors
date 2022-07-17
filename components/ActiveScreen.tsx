4c84aba395afef9b992acad0e9cd549a3b2fa621
Anton Yanovskyi <3898110@gmail.com>
2022-07-17 18:37:41 +0300

    fix: - change interfaces

--- components/ActiveScreen.tsx
+++ components/ActiveScreen.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import StartScreen from './StartScreen';
 import PlayScreen from './PlayScreen';
 
 import { IActiveScreen } from '../helpers/interfaces';
 
-const ActiveScreen = ({ activeMode }: IActiveScreen) => {
+const ActiveScreen: FC<IActiveScreen> = ({ activeMode }) => {
   if (activeMode !== null) {
     return <PlayScreen />;
   }
